6eab866da06dbb82eb85425ce0747e6a0ffc77be
Aleks Sarkisyan <webi.aleks@gmail.com>
2023-03-27 16:42:37 +0300

    FA-6-user-service-changes Adds gitignore file

--- server/shared-service/dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { UserCommands, ErrorMessages, Database } from "./enums/user.enum";
-export { UserCommands, ErrorMessages, Database };
-//# sourceMappingURL=index.d.ts.map
--- server/shared-service/dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Database = exports.ErrorMessages = exports.UserCommands = void 0;
-const user_enum_1 = require("./enums/user.enum");
-Object.defineProperty(exports, "UserCommands", { enumerable: true, get: function () { return user_enum_1.UserCommands; } });
-Object.defineProperty(exports, "ErrorMessages", { enumerable: true, get: function () { return user_enum_1.ErrorMessages; } });
-Object.defineProperty(exports, "Database", { enumerable: true, get: function () { return user_enum_1.Database; } });
-//# sourceMappingURL=index.js.map
